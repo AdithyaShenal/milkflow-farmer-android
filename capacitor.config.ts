132ac60c7a0643c9d756cd92e8e4062cec7395ea
Adithya Shenal <washenal55@gmail.com>
2026-02-09 00:46:11 +0530

    android settings done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.adithyashenal.app",
+  appId: "com.dairyconnect.app",
   appName: "Dairy Connect",
   webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
   plugins: {
     CapacitorHttp: {
       enabled: true,
